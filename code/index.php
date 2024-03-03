00f9c098f441fb372d045bc5186b1dc7976e4705
DimitriiTrater <dima1603040@gmail.com>
2024-03-03 17:27:26 +0200

    [lab_2][task_1] Ref access

--- code/index.php
+++ code/index.php
@@ -1,2 +1,23 @@
-<?php
-echo phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <hr>
+    Task 1: <br>
+    <?php
+        $very_bad_unclear_name = "15 chicken wings";
+
+        $order = &$very_bad_unclear_name;
+
+        $order .= " some string";
+    
+        echo "\nYour order is: $very_bad_unclear_name.";
+    ?>
+    <hr>
+</body>
+</html>
+
