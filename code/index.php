8ce04fb078fc7178cff45416c394d10d73463fae
DimitriiTrater <dima1603040@gmail.com>
2024-03-08 15:11:28 +0200

    [lab_2][task_2] Numbers

--- code/index.php
+++ code/index.php
@@ -20,6 +20,19 @@
         echo "\nYour order is: $very_bad_unclear_name.";
     ?>
     <hr>
+    Task 2: <br>
+    <?php
+        $any_name_int = 9;
+        echo $any_name_int;
+        echo '\n';
+        $any_name_float = 9.23;
+        echo $any_name_float;
+        echo 12;
+        $last_month = 1089.98;
+        $this_month = 1187.23;
+        echo $last_month - $this_month;
+    ?>
+    <hr>
 </body>
 </html>
 
